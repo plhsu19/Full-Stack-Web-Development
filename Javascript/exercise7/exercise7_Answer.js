447966fafa8ad6500485c8a9777ed33cbc733f5f
PLHsu <54821322+plhsu19@users.noreply.github.com>
2020-08-06 23:21:21 +0200

    JS syntax: loops, Object

---
+++ Javascript/exercise7/exercise7_Answer.js
@@ -0,0 +1,26 @@
+// Create an object and an array which we will use in our facebook exercise. 
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+var user1 =  {
+    username: "badass",
+    password: "d390sf9"
+}
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+var database = [user1];
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+var newsfeed = [
+    {
+        username: "apath",
+        timeline: "03-03-2019"
+    },
+    {
+        username: "aka",
+        timeline: "23-03-2039"
+    },
+    {
+        username: "red",
+        timeline: "03-03-2007"
+    }
+]
--- Javascript/script.js
+++ Javascript/script.js
@@ -1,18 +1,60 @@
-var user = {
-    name: 'Pei',
-    age: 31,
-    gender: 'male',
-    isMarried: false,
-    spell: ['pirika', 'gogo'],
-    shout: function() {
-        alert('Ahhhhh');
-    }
-};
+var toDos = [
+    'clean room',
+    'practive JS',
+    'exercise',
+    'wash teeth',
+    'eat healthy'
+];
+var toDos2 = [
+    'clean room!',
+    'practive JS!',
+    'exercise!',
+    'wash teeth!',
+    'eat healthy!'
+];
+var len = toDos.length;
+// for (var i = 0; i < len; i++) {
+//     toDo.pop();
+// }
 
-var a = [user, {
-    name: 'jess',
-    age: 45
-}]
+// var count = 10;
+// while (count > 10) {
+//     console.log('while' + count);
+//     count -= 1;
+// }
 
-user2 = {};
-user3 = null;
+// var count2 = 10;
+// do {
+//     console.log(count2);
+//     console.log('this is in do statememt'); 
+//     count2 -= 1;
+// } while (count2 > 10)
+function lambda(toDo, i) {
+    console.log(toDo, i);
+}
+
+toDos.forEach(lambda);
+toDos2.forEach(lambda);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
