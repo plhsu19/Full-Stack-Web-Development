570bf2ea96be7ba60db17d6384574b209c986cdb
PLHsu <54821322+plhsu19@users.noreply.github.com>
2020-08-07 12:54:47 +0200

    start DOM Manipulation

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,60 +1,52 @@
-var toDos = [
-    'clean room',
-    'practive JS',
-    'exercise',
-    'wash teeth',
-    'eat healthy'
+var dataBase = [
+    {
+        username: "Panda",
+        password: "mikichan112"
+    },
+    {
+        username: "Ice Bear",
+        password: "icebear"
+    },
+    {
+        username: "Grizzly",
+        password: "hamburger"
+    }
 ];
-var toDos2 = [
-    'clean room!',
-    'practive JS!',
-    'exercise!',
-    'wash teeth!',
-    'eat healthy!'
+
+var newsFeed = [
+    {
+        username: 'Ice Bear',
+        timeline: 'Ice bear wants some fish'
+    },
+    {
+        username: 'Grizzly',
+        timeline: 'It\'s getting hot out there~~~'
+    },
+    {
+        username: "Panda",
+        timeline: "looking for a dating partner : )"
+    }
 ];
-var len = toDos.length;
-// for (var i = 0; i < len; i++) {
-//     toDo.pop();
-// }
 
-// var count = 10;
-// while (count > 10) {
-//     console.log('while' + count);
-//     count -= 1;
-// }
-
-// var count2 = 10;
-// do {
-//     console.log(count2);
-//     console.log('this is in do statememt'); 
-//     count2 -= 1;
-// } while (count2 > 10)
-function lambda(toDo, i) {
-    console.log(toDo, i);
+function isValid(user, pass) {
+    for (var i = 0; i < dataBase.length; i++) {
+        if (user === dataBase[i].username && pass === dataBase[i].password) {
+            return true;
+        }
+    }
+    return false;
 }
 
-toDos.forEach(lambda);
-toDos2.forEach(lambda);
+function signIn(user, pass) {
+    if (isValid(user, pass)) {
+        console.log(newsFeed);
+    } else {
+        alert('Sorry, wrong username or pass word!')
+    }
+}
 
 
+var user = prompt('Enter the username: ');
+var pass = prompt('Enter the password: ');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+signIn(user, pass);
