ca4a763911bd1c8e23824f2f88f163e8e5d89481
PLHsu <54821322+plhsu19@users.noreply.github.com>
2020-08-16 16:28:06 +0200

    advanced control flow

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,9 +1,28 @@
-var fun = 'global gun';
-
-function firstfunc() {
-    function sencondfunc(){
-        console.log(fun);
-    }
+function isValid(bool) {
+    return bool;
 }
+// var passWord = isValid(true) ? '1234' : 'access denied';
 
-firstfunc();
+var passRequest = 'your password is: ' + (isValid(false) ? '1234' : 'access denied');
+console.log(passRequest);
+
+function action(command) {
+    var result;
+    switch(command) {
+        case 'right':
+            result = 'meet a slime';
+            break;
+        case 'left':
+            result = 'pick up a sword';
+            break;
+        case 'up':
+            result = 'run away';
+            break;
+        case 'down':
+            result = 'combat';
+            break;
+        default:
+            result = 'please enter valid command'
+    }
+    return result;
+}
