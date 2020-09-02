3bc9064e17dd8b46acf519cfc31072c60550242c
PLHsu <54821322+plhsu19@users.noreply.github.com>
2020-09-02 21:13:29 +0200

    asynchrous programming

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,29 +1,6 @@
-const basket = ['apples', 'oranges', 'grapes'];
-const detailedBasket = {
-    apples: 5,
-    oranges: 10,
-    grapes: 100,
-}
-// 1
-for (let i = 0; i < basket.length; i++) {
-    console.log(basket[i]);
-}
-
-// 2
-basket.forEach((item) => {
-    console.log(item);
-})
-
-// for-of 
-for (item of basket) {
-    console.log(item);
-}
-
-for (item of 'iterable') {
-    console.log(item)
-}
-
-// for-in
-for (item in detailedBasket) {
-    console.log(item);
-}
+// asynchronous programming
+console.log('1');
+setTimeout(() => {
+    console.log('2')
+}, 2000)
+console.log('3');
