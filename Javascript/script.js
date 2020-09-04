e6a9b98489bfd09bc56a379dcfcc062fd8308b82
PLHsu <54821322+plhsu19@users.noreply.github.com>
2020-09-04 11:01:20 +0200

    asynchronous in JS

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,6 +1,20 @@
-// asynchronous programming
+// call stack
 console.log('1');
 setTimeout(() => {
     console.log('2')
 }, 2000)
 console.log('3');
+
+
+// CALL STACK
+
+// Web API
+
+
+// callback queue
+
+// event loop
+
+window.addEventListener('click', () => {
+    console.log('clikc')
+})
