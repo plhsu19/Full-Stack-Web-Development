ca63f65bc5149e118c5d57c20f2f2c7dd7163296
PLHsu <54821322+plhsu19@users.noreply.github.com>
2020-09-25 22:06:47 +0200

    JS code challenges_q1_bonus

--- Javascript/Coding_Challenge6/challenge1.js
+++ Javascript/Coding_Challenge6/challenge1.js
@@ -3,14 +3,17 @@
 // For example answer(ArrayFromAbove) should return: [[1,1,1,1],[2,2,2], 4,5,10,[20,20], 391, 392,591]. 
 // Bonus: Make it so it organizes strings differently from number types. i.e. [1, "2", "3", 2] should return [[1,2], ["2", "3"]]
 
-// Standard Solution
+
+//test unit
+let testArray = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
+console.log(solution(testArray));
+
+// standard solution
 function solution(array) {
     let answer = [];
     let sortedArray = array.slice().sort((a, b) => a - b);
-    console.log(sortedArray);
 
     let pre = sortedArray[0];
-    console.log(sortedArray[0])
     let tempArray = [];
 
     for (num of sortedArray) {
@@ -39,6 +42,28 @@ function solution(array) {
     return answer;
 }
 
-//test unit
-let testArray = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
-console.log(solution(testArray));
+
+// Bonus: Make it so it organizes strings differently from number types. i.e. [1, "2", "3", 2] should return [[1,2], ["2", "3"]]
+// test unit
+let testArrayBonus = [1, "2", "3", 2, 8, 100, 4, "80", 9, "9", "4", 3, 10, 100, 8]
+let answerArray = solutionBonus(testArrayBonus)
+console.log(answerArray)
+
+// solution bonus
+function solutionBonus(arr) {
+    // final answer array
+    let answerArray = []
+
+    // sort the array using numerical order no matter string or number
+    let arraySorted = arr.sort((a, b) => a - b)
+
+    // filter the number array and string array
+    let numArray = arraySorted.filter(elem => typeof(elem) === "number")
+    let strArray = arraySorted.filter(elem => typeof(elem) === "string" )
+
+    // add 2 arrays into the answer array
+    // answerArray.splice(0, 0, numArray, strArray)
+    answerArray.push(numArray, strArray)
+
+    return answerArray
+}
