6c6655b4b2964ab7b6872725f095ce0affc172cb
PLHsu <54821322+plhsu19@users.noreply.github.com>
2021-01-20 22:29:44 +0100

    Build RESTful API: access informations of requests

--- Node/server.js
+++ Node/server.js
@@ -11,9 +11,19 @@ const pokemons = {
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
-app.post('/profile', (req, res) => {
-    console.log(req.body);
-    res.send('success');
+app.get('/:id', (req, res) => {
+    // access the query in the GET request
+    // console.log(req.query); 
+
+    // access the header of the request
+    // console.log(req.headers);
+
+    // access the parameter values of the URL => deprecated?
+    console.log(req.params);
+
+    // access the body of the request, pre-processed by middlewares
+    // console.log(req.body );
+    res.send('access root');
 })
 
 app.listen(3000)
