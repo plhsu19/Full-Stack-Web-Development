f9a34054b4891d6d000ae80b9e1c838f7d597ba2
PLHsu <54821322+plhsu19@users.noreply.github.com>
2021-01-21 17:54:02 +0100

    build a simple static server

--- Node/server.js
+++ Node/server.js
@@ -1,29 +1,9 @@
-// build a simple server using express
-import express from 'express';
+// build a simple RESTful API server using express
+const express = require('express')
+// import express from 'express';
 
 const app = express();
 
-const pokemons = {
-    pikachu: 'electircal_attack',
-}
-// middleware to parse the body of message in different form
-// eg, urlencoded, raw(JSON)
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
-
-app.get('/:id', (req, res) => {
-    // access the query in the GET request
-    // console.log(req.query); 
-
-    // access the header of the request
-    // console.log(req.headers);
-
-    // access the parameter values of the URL => deprecated?
-    console.log(req.params);
-
-    // access the body of the request, pre-processed by middlewares
-    // console.log(req.body );
-    res.send('access root');
-})
+app.use(express.static(__dirname + '/public'))
 
 app.listen(3000)
